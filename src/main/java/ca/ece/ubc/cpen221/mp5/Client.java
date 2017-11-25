e5f62c6a38e257677cd8bb2369c247d5e52c1094
Kota Chang <i@dongju.org>
2017-11-24 17:55:55 -0800

    Server Class Created

---
+++ src/main/java/ca/ece/ubc/cpen221/mp5/Client.java
@@ -0,0 +1,18 @@
+package ca.ece.ubc.cpen221.mp5;
+
+public class Client {
+
+	YelpDBServer newServer = new YelpDBServer(4949);
+	Thread thread;
+
+	public Client() {
+		thread = new Thread(newServer);
+		thread.start();
+	}
+	
+	try {
+		
+	} catch (InterruptedException e) {
+		
+	}
+}
---
+++ src/main/java/ca/ece/ubc/cpen221/mp5/YelpDBServer.java
@@ -0,0 +1,21 @@
+package ca.ece.ubc.cpen221.mp5;
+
+import java.net.ServerSocket;
+
+public class YelpDBServer implements Runnable{
+	protected int          serverPortVal   = 4949;
+    protected ServerSocket serverSocketVal = null;
+    protected boolean      hasStopped    = false;
+    protected Thread       movingThread= null;
+
+	YelpDBServer (int port){
+		this.serverPortVal = port;
+	}
+	
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
