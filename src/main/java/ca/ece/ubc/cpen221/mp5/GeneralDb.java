8c7462012cf0f5f2a6d1a94f8b57601c5c8dd339
jyarschenko <jordanyarschenko@yahoo.com>
2017-11-24 16:38:12 -0800

    Added type parameters to classes

--- src/main/java/ca/ece/ubc/cpen221/mp5/GeneralDb.java
+++ src/main/java/ca/ece/ubc/cpen221/mp5/GeneralDb.java
@@ -1,5 +1,26 @@
 package ca.ece.ubc.cpen221.mp5;
 
-public class GeneralDb {
+import java.util.Set;
+import java.util.function.ToDoubleBiFunction;
+
+public class GeneralDb<T> implements MP5Db<T>{
+
+	@Override
+	public Set getMatches(String queryString) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String kMeansClusters_json(int k) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ToDoubleBiFunction getPredictorFunction(String user) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
--- src/main/java/ca/ece/ubc/cpen221/mp5/YelpDB.java
+++ src/main/java/ca/ece/ubc/cpen221/mp5/YelpDB.java
@@ -1,5 +1,5 @@
 package ca.ece.ubc.cpen221.mp5;
 
-public class YelpDB extends GeneralDb {
+public class YelpDB<T> extends GeneralDb<T> {
 
 }
