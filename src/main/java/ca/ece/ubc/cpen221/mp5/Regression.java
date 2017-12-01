4a619747f7f2446dc90921bbe8882142420831a8
Kota Chang <i@dongju.org>
2017-11-30 22:56:36 -0800

    temporary class added

---
+++ src/main/java/ca/ece/ubc/cpen221/mp5/Regression.java
@@ -0,0 +1,16 @@
+package ca.ece.ubc.cpen221.mp5;
+
+import java.util.function.ToDoubleBiFunction;
+
+public class Regression {
+	String businessId = "";
+	Review review = new Review(businessId);
+
+	/**
+	 * 
+	 */
+	public ToDoubleBiFunction getPredictorFunction(String user) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+}
