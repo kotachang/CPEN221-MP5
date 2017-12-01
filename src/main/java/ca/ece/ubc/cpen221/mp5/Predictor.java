2c877cf247d974d4830e72b59d130a7ed1e01128
Kota Chang <i@dongju.org>
2017-11-30 23:25:30 -0800

    added classes for regression

---
+++ src/main/java/ca/ece/ubc/cpen221/mp5/Predictor.java
@@ -0,0 +1,15 @@
+package ca.ece.ubc.cpen221.mp5;
+
+import java.util.function.ToDoubleBiFunction;
+
+public class Predictor<T> implements ToDoubleBiFunction<MP5Db<T>,String>{
+
+	@Override
+	public double applyAsDouble(MP5Db<T> t, String u) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	
+
+}
--- src/main/java/ca/ece/ubc/cpen221/mp5/TemporaryClass.java
+++ src/main/java/ca/ece/ubc/cpen221/mp5/TemporaryClass.java
@@ -3,12 +3,21 @@
  */
 package ca.ece.ubc.cpen221.mp5;
 
+import java.util.function.ToDoubleBiFunction;
+
 /**
  * 
  *
  */
 public class TemporaryClass {
-	
+	/**
+	 * 
+	 */
+
+	public ToDoubleBiFunction getPredictorFunction(String user) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 	
 
 }
