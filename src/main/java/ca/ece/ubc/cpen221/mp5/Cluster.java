f469d96218afd57194cf135c4257ce977737ea21
jyarschenko <jordanyarschenko@yahoo.com>
2017-12-02 23:29:57 -0800

    Remove unused imports

--- src/main/java/ca/ece/ubc/cpen221/mp5/Cluster.java
+++ src/main/java/ca/ece/ubc/cpen221/mp5/Cluster.java
@@ -1,10 +1,7 @@
 package ca.ece.ubc.cpen221.mp5;
 
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
-import java.util.Map;
 import java.util.Set;
 
 /**
--- src/main/java/ca/ece/ubc/cpen221/mp5/Predictor.java
+++ src/main/java/ca/ece/ubc/cpen221/mp5/Predictor.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.function.ToDoubleBiFunction;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 /**
  * @RI: predicted rating value is for the specific user, and the specific
