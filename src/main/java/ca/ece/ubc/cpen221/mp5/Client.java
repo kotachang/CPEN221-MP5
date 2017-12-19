b2946b46d17a9c9c75b18cbb2a529908ae69a4cf
jyarschenko <jordanyarschenko@yahoo.com>
2017-12-18 16:16:29 -0800

    Client stuff

--- src/main/java/ca/ece/ubc/cpen221/mp5/Client.java
+++ src/main/java/ca/ece/ubc/cpen221/mp5/Client.java
@@ -1,13 +1,60 @@
 package ca.ece.ubc.cpen221.mp5;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.Scanner;
+
 public class Client {
 
 	YelpDBServer newServer = new YelpDBServer(4949);
 	Thread thread;
+	private Socket socket;
+	private BufferedReader in;
+	private PrintWriter out;
 
-	public Client() {
-		thread = new Thread(newServer);
-		thread.start();
+	public Client(String hostname, int port) throws UnknownHostException, IOException {
+		socket = new Socket(hostname, port);
+		in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+		out = new PrintWriter(new OutputStreamWriter(socket.getOutputStream()));
+	}
+	
+	public void sendRequest(String request) {
+		out.print(request);
+		out.flush();
+	}
+	
+	public String getReply() throws IOException {
+		String reply = in.readLine();
+		if(reply == null) {
+			throw new IOException("connection terminated unexpectedly");
+		}
+		return reply;
+	}
+	
+	public void close() throws IOException {
+		in.close();
+		out.close();
+		socket.close();
+	}
+	
+	public static void main(String[] args) {
+		try {
+			Client client = new Client("localhost", 4949);
+			Scanner scanner = new Scanner(System.in);
+			System.out.println("Request:");
+			String request = scanner.next();
+			
+			client.sendRequest(request);
+			System.out.println(client.getReply());
+		}
+		catch(IOException e) {
+			e.printStackTrace();
+		}
 	}
 
 }
--- src/test/java/ca/ece/ubc/cpen221/mp5/tests/ca/ece/ubc/cpen221/mp5/tests/ServerTests.java
+++ src/test/java/ca/ece/ubc/cpen221/mp5/tests/ca/ece/ubc/cpen221/mp5/tests/ServerTests.java
@@ -1,11 +1,18 @@
 package ca.ece.ubc.cpen221.mp5.tests.ca.ece.ubc.cpen221.mp5.tests;
 
+import java.io.IOException;
+
 import org.junit.Test;
+
+import ca.ece.ubc.cpen221.mp5.Client;
+import ca.ece.ubc.cpen221.mp5.YelpDBServer;
 
 public class ServerTests {
 
 	@Test
-	public void serverTest1() {
-		
+	public void serverTest1() throws IOException {
+		Client client1 = new Client("localhost", 4949);
+		Client.main(new String[]{""});
+		YelpDBServer server = new YelpDBServer(4949);
 	}
 }
