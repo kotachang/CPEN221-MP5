6ac984e6452087e2aebbf6e8183017cc24bde508
jyarschenko <jordanyarschenko@yahoo.com>
2017-12-20 18:12:18 -0800

    Tests for server done

--- src/main/java/ca/ece/ubc/cpen221/mp5/YelpDBServer.java
+++ src/main/java/ca/ece/ubc/cpen221/mp5/YelpDBServer.java
@@ -8,20 +8,25 @@ import java.io.PrintWriter;
 import java.io.StringReader;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.net.SocketException;
 
 import javax.json.Json;
 import javax.json.JsonObject;
 import javax.json.JsonString;
 import javax.json.stream.JsonParserFactory;
 
-public class YelpDBServer extends java.lang.Thread{
+public class YelpDBServer extends java.lang.Thread {
 	/** Default port number where the server listens for connections. */
 	public static final int YelpDBServer_PORT = 4949;
 	private static YelpDB database;
 
 	private ServerSocket serverSocket;
 	private static int userIds = 1;
+	private static int restuarantIds = 1;
+	private static int reviewIds = 1;
+
 	private volatile boolean isRunning;
+	private volatile boolean isClosed;
 
 	// Rep invariant: serverSocket != null
 	//
@@ -33,7 +38,7 @@ public class YelpDBServer extends java.lang.Thread{
 	// TODO data in handle()
 
 	/**
-	 * Make a FibonacciServerMulti that listens for connections on port.
+	 * Make a YelpDBServer that listens for connections on port.
 	 * 
 	 * @param port
 	 *            port number, requires 0 <= port <= 65535
@@ -53,31 +58,35 @@ public class YelpDBServer extends java.lang.Thread{
 	 *             if the main server socket is broken
 	 */
 	public void serve() throws IOException {
-		isRunning = false;
-		while (!isRunning) {
+		isRunning = true;
+		while (isRunning) {
 			// block until a client connects
-			final Socket socket = serverSocket.accept();
-			// create a new thread to handle that client
-			Thread handler = new Thread(new Runnable() {
-				public void run() {
-					try {
+			try {
+				final Socket socket = serverSocket.accept();
+				// create a new thread to handle that client
+				Thread handler = new Thread(new Runnable() {
+					public void run() {
 						try {
-							handle(socket);
-						} finally {
-							isRunning = true;
-							socket.close();
-							
+							try {
+								handle(socket);
+							} finally {
+								isRunning = false;
+								socket.close();
+
+							}
+						} catch (IOException ioe) {
+							// this exception wouldn't terminate serve(),
+							// since we're now on a different thread, but
+							// we still need to handle it
+							ioe.printStackTrace();
 						}
-					} catch (IOException ioe) {
-						// this exception wouldn't terminate serve(),
-						// since we're now on a different thread, but
-						// we still need to handle it
-						ioe.printStackTrace();
 					}
-				}
-			});
-			// start the thread
-			handler.start();
+				});
+				// start the thread
+				handler.start();
+			} catch (SocketException e) {
+				break;
+			}
 		}
 	}
 
@@ -96,7 +105,7 @@ public class YelpDBServer extends java.lang.Thread{
 		// that convert it from a byte stream to a character stream,
 		// and that buffer it so that we can read a line at a time
 		BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-				
+
 		// similarly, wrap character=>bytestream converter around the
 		// socket output stream, and wrap a PrintWriter around that so
 		// that we have more convenient ways to write Java primitive
@@ -116,25 +125,26 @@ public class YelpDBServer extends java.lang.Thread{
 						String business = YelpDBServer.getRestaurant(request[1]);
 						out.println(business);
 						out.flush();
+						line = null;
 					} else if (request[0].equals("ADDUSER")) {
 						out.println(YelpDBServer.addUser(request[1]));
 						out.flush();
+						line = null;
 					} else if (request[0].equals("ADDRESTAURANT")) {
 						out.println(YelpDBServer.addRestarant(request[1]));
 						out.flush();
+						line = null;
 					} else if (request[0].equals("ADDREVIEW")) {
 						out.println(YelpDBServer.addReview(request[1]));
 						out.flush();
-					}
-					else {
+						line = null;
+					} else {
 						System.err.println("ERR: ILLEGAL_REQUEST");
+						out.println("");
+						out.flush();
+						line = null;
 					}
 
-					/*
-					 * int x = Integer.valueOf(line); // compute answer and send back to client
-					 * BigInteger y = fibonacci(x); System.err.println("reply: " + y);
-					 * out.println(y);
-					 */
 				} catch (NumberFormatException e) {
 					// complain about ill-formatted request
 					System.err.println("reply: err");
@@ -184,6 +194,9 @@ public class YelpDBServer extends java.lang.Thread{
 	public synchronized static String addRestarant(String restaurantInfo) {
 		JsonObject restaurantData = Json.createReader(new StringReader(restaurantInfo)).readObject();
 		try {
+			restaurantData = Json.createObjectBuilder(restaurantData).add("business_id", "" + restuarantIds)
+					.add("stars", "0.0").build();
+			restuarantIds++;
 			Restaurant newRestaurant = (Restaurant) database.parseBusiness(restaurantData);
 			database.addBusiness(newRestaurant);
 			return newRestaurant.getJson().toString();
@@ -196,19 +209,20 @@ public class YelpDBServer extends java.lang.Thread{
 	public synchronized static String addReview(String review) {
 		JsonObject reviewData = Json.createReader(new StringReader(review)).readObject();
 		try {
+			reviewData = Json.createObjectBuilder(reviewData).add("review_id", "" + reviewIds).build();
+			reviewIds++;
 			Review newReview = database.parseReview(reviewData);
-			if(newReview == null) {
+			if (newReview == null) {
 				String s = reviewData.getString("user_id");
-				if(!database.getUsers().contains(new User(s))) {
+				if (!database.getUsers().contains(new User(s))) {
 					System.err.println("ERR: NO_SUCH_USER");
 				}
 				s = reviewData.getString("business_id");
-				if(!database.getBusinesses().contains(new Business(s))) {
-					System.err.println("ERR: NO_SUCH_ RESTAURANT");
+				if (!database.getBusinesses().contains(new Business(s))) {
+					System.err.println("ERR: NO_SUCH_RESTAURANT");
 				}
 				return null;
-			}
-			else {
+			} else {
 				database.addReview(newReview);
 				return newReview.getJson().toString();
 			}
@@ -231,13 +245,18 @@ public class YelpDBServer extends java.lang.Thread{
 			e.printStackTrace();
 		}
 	}
-	
+
 	public void run() {
 		try {
 			this.serve();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
+	}
+
+	public void close() throws InterruptedException, IOException {
+		isRunning = false;
+		serverSocket.close();
 	}
 
 }
--- src/test/java/ca/ece/ubc/cpen221/mp5/tests/ca/ece/ubc/cpen221/mp5/tests/ServerTests.java
+++ src/test/java/ca/ece/ubc/cpen221/mp5/tests/ca/ece/ubc/cpen221/mp5/tests/ServerTests.java
@@ -3,7 +3,9 @@ package ca.ece.ubc.cpen221.mp5.tests.ca.ece.ubc.cpen221.mp5.tests;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.fail;
 
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.PrintStream;
 import java.util.Scanner;
 
 import org.junit.Test;
@@ -33,12 +35,362 @@ public class ServerTests {
 			Client client = new Client("localhost", 4949);
 			client.sendRequest(request);
 			reply = client.getReply();
-			
+
 			client.close();
+			server.close();
 
 		} catch (IOException e) {
 			fail();
 		}
-		assertEquals(reply, "{\"url\":\"http://www.yelp.com/user_details?userid=1\",\"votes\":\"{}\",\"review_count\":0,\"type\":\"user\",\"user_id\":\"1\",\"name\":\"New User\",\"average_stars\":0.0}");
+		assertEquals(reply,
+				"{\"url\":\"http://www.yelp.com/user_details?userid=1\",\"votes\":\"{}\",\"review_count\":0,\"type\":\"user\",\"user_id\":\"1\",\"name\":\"New User\",\"average_stars\":0.0}");
+	}
+
+	@Test
+	public void serverTest2() throws IOException, InterruptedException {
+
+		String request = "ADDRESTAURANT {\"open\": true, \"url\": \"http://www.yelp.com/biz/some-thai-berkeley\", \"longitude\": -122.2602981, \"neighborhoods\": [\"UC Campus Area\"], \"name\": \"Some Thai\", \"categories\": [\"Thai\", \"Restaurants\"], \"state\": \"CA\", \"type\": \"business\", \"city\": \"Berkeley\", \"full_address\": \"1995 Euclid Ave\\nUC Campus Area\\nBerkeley, CA 94709\", \"review_count\": 0, \"photo_url\": \"http://s3-media2.ak.yelpcdn.com/bphoto/1/2.jpg\", \"schools\": [\"University of California at Berkeley\"], \"latitude\": 37.8759615, \"price\": 2}";
+
+		String reply = null;
+		try {
+			final YelpDBServer server = new YelpDBServer(4949);
+			Thread serverThread = new Thread(() -> {
+				try {
+					server.serve();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			});
+			serverThread.start();
+
+			Client client = new Client("localhost", 4949);
+			client.sendRequest(request);
+			reply = client.getReply();
+
+			client.close();
+			server.close();
+
+		} catch (IOException e) {
+			fail();
+		}
+		assertEquals(reply,
+				"{\"open\":true,\"url\":\"http://www.yelp.com/biz/some-thai-berkeley\",\"longitude\":-122.2602981,\"neighborhoods\":[\"UC Campus Area\"],\"name\":\"Some Thai\",\"categories\":[\"Thai\",\"Restaurants\"],\"state\":\"CA\",\"type\":\"business\",\"city\":\"Berkeley\",\"full_address\":\"1995 Euclid Ave\\nUC Campus Area\\nBerkeley, CA 94709\",\"review_count\":0,\"photo_url\":\"http://s3-media2.ak.yelpcdn.com/bphoto/1/2.jpg\",\"schools\":[\"University of California at Berkeley\"],\"latitude\":37.8759615,\"price\":2,\"business_id\":\"1\",\"stars\":\"0.0\"}");
+	}
+
+	@Test
+	public void serverTest3() throws IOException, InterruptedException {
+
+		String request = "ADDREVIEW {\"type\": \"review\", \"business_id\": \"1CBs84C-a-cuA3vncXVSAw\", \"votes\": {\"cool\": 0, \"useful\": 0, \"funny\": 0}, \"text\": \"The pizza is terrible, but if you need a place to watch a game or just down some pitchers, this place works.\\n\\nOh, and the pasta is even worse than the pizza.\", \"stars\": 1, \"user_id\": \"90wm_01FAIqhcgV_mPON9Q\", \"date\": \"2017-12-20\"}";
+
+		String reply = null;
+		try {
+			final YelpDBServer server = new YelpDBServer(4949);
+			Thread serverThread = new Thread(() -> {
+				try {
+					server.serve();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			});
+			serverThread.start();
+
+			Client client = new Client("localhost", 4949);
+			client.sendRequest(request);
+			reply = client.getReply();
+
+			client.close();
+			server.close();
+
+		} catch (IOException e) {
+			fail();
+		}
+		assertEquals(reply,
+				"{\"type\":\"review\",\"business_id\":\"1CBs84C-a-cuA3vncXVSAw\",\"votes\":{\"cool\":0,\"useful\":0,\"funny\":0},\"text\":\"The pizza is terrible, but if you need a place to watch a game or just down some pitchers, this place works.\\n\\nOh, and the pasta is even worse than the pizza.\",\"stars\":1,\"user_id\":\"90wm_01FAIqhcgV_mPON9Q\",\"date\":\"2017-12-20\",\"review_id\":\"1\"}");
+	}
+
+	@Test
+	public void serverTest4() throws IOException, InterruptedException {
+
+		String request = "GETRESTAURANT gclB3ED6uk6viWlolSb_uA";
+
+		String reply = null;
+		try {
+			final YelpDBServer server = new YelpDBServer(4949);
+			Thread serverThread = new Thread(() -> {
+				try {
+					server.serve();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			});
+			serverThread.start();
+
+			Client client = new Client("localhost", 4949);
+			client.sendRequest(request);
+			reply = client.getReply();
+
+			client.close();
+			server.close();
+
+		} catch (IOException e) {
+			fail();
+		}
+		assertEquals(reply,
+				"{\"open\":true,\"url\":\"http://www.yelp.com/biz/cafe-3-berkeley\",\"longitude\":-122.260408,\"neighborhoods\":[\"Telegraph Ave\",\"UC Campus Area\"],\"business_id\":\"gclB3ED6uk6viWlolSb_uA\",\"name\":\"Cafe 3\",\"categories\":[\"Cafes\",\"Restaurants\"],\"state\":\"CA\",\"type\":\"business\",\"stars\":2.0,\"city\":\"Berkeley\",\"full_address\":\"2400 Durant Ave\\nTelegraph Ave\\nBerkeley, CA 94701\",\"review_count\":9,\"photo_url\":\"http://s3-media1.ak.yelpcdn.com/bphoto/AaHq1UzXiT6zDBUYrJ2NKA/ms.jpg\",\"schools\":[\"University of California at Berkeley\"],\"latitude\":37.867417,\"price\":1}");
+	}
+
+	@Test
+	public void serverTest5() throws IOException, InterruptedException {
+
+		String request = "GETRESTAURANT 1";
+
+		ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+		System.setErr(new PrintStream(outContent));
+
+		String reply = null;
+		try {
+			final YelpDBServer server = new YelpDBServer(4949);
+			Thread serverThread = new Thread(() -> {
+				try {
+					server.serve();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			});
+			serverThread.start();
+
+			Client client = new Client("localhost", 4949);
+			client.sendRequest(request);
+			reply = client.getReply();
+
+			client.close();
+			server.close();
+
+		} catch (IOException e) {
+			fail();
+		}
+		String nullString = "null";
+		assertEquals(reply, nullString);
+		assertEquals(outContent.toString(),
+				"client connected\r\n" + "request: GETRESTAURANT 1\r\n" + "ERR: NO_SUCH_RESTAURANT\r\n");
+	}
+
+	@Test
+	public void serverTest6() throws IOException, InterruptedException {
+
+		String request = "ADDUSER {\"nme\": \"Me\"}";
+
+		ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+		System.setErr(new PrintStream(outContent));
+
+		String reply = null;
+		try {
+			final YelpDBServer server = new YelpDBServer(4949);
+			Thread serverThread = new Thread(() -> {
+				try {
+					server.serve();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			});
+			serverThread.start();
+
+			Client client = new Client("localhost", 4949);
+			client.sendRequest(request);
+			reply = client.getReply();
+
+			client.close();
+			server.close();
+
+		} catch (IOException e) {
+			fail();
+		}
+		String nullString = "null";
+		assertEquals(reply, nullString);
+		assertEquals(outContent.toString(),
+				"client connected\r\n" + "request: ADDUSER {\"nme\": \"Me\"}\r\n" + "ERR: INVALID_USER_STRING\r\n");
+	}
+
+	@Test
+	public void serverTest7() throws IOException, InterruptedException {
+
+		String request = "ADDRESTAURANT {\"nme\": \"Me\"}";
+
+		ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+		System.setErr(new PrintStream(outContent));
+
+		String reply = null;
+		try {
+			final YelpDBServer server = new YelpDBServer(4949);
+			Thread serverThread = new Thread(() -> {
+				try {
+					server.serve();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			});
+			serverThread.start();
+
+			Client client = new Client("localhost", 4949);
+			client.sendRequest(request);
+			reply = client.getReply();
+
+			client.close();
+			server.close();
+
+		} catch (IOException e) {
+			fail();
+		}
+		String nullString = "null";
+		assertEquals(reply, nullString);
+		assertEquals(outContent.toString(), "client connected\r\n" + "request: ADDRESTAURANT {\"nme\": \"Me\"}\r\n"
+				+ "ERR: INVALID_RESTAURANT_STRING\r\n");
+	}
+
+	@Test
+	public void serverTest8() throws IOException, InterruptedException {
+
+		String request = "ADDREVIEW {\"nme\": \"Me\"}";
+
+		ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+		System.setErr(new PrintStream(outContent));
+
+		String reply = null;
+		try {
+			final YelpDBServer server = new YelpDBServer(4949);
+			Thread serverThread = new Thread(() -> {
+				try {
+					server.serve();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			});
+			serverThread.start();
+
+			Client client = new Client("localhost", 4949);
+			client.sendRequest(request);
+			reply = client.getReply();
+
+			client.close();
+			server.close();
+
+		} catch (IOException e) {
+			fail();
+		}
+		String nullString = "null";
+		assertEquals(reply, nullString);
+		assertEquals(outContent.toString(),
+				"client connected\r\n" + "request: ADDREVIEW {\"nme\": \"Me\"}\r\n" + "ERR: INVALID_REVIEW_STRING\r\n");
+	}
+
+	@Test
+	public void serverTest9() throws IOException, InterruptedException {
+
+		String request = "ADDREVIEW {\"type\": \"review\", \"business_id\": \"1CBs84C-a-cuA3vncXVSAw\", \"votes\": {\"cool\": 0, \"useful\": 0, \"funny\": 0}, \"review_id\": \"0f8QNSVSocn40zr1tSSGRw\", \"text\": \"Food here is very consistent and good for a quick lunch or dinner. I usually order the garlic bread and salad; however, their pizza, especially the bbq chicken, is pretty good too.\", \"stars\": 4, \"user_id\": \"7\", \"date\": \"2012-01-28\"}";
+
+		ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+		System.setErr(new PrintStream(outContent));
+
+		String reply = null;
+		try {
+			final YelpDBServer server = new YelpDBServer(4949);
+			Thread serverThread = new Thread(() -> {
+				try {
+					server.serve();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			});
+			serverThread.start();
+
+			Client client = new Client("localhost", 4949);
+			client.sendRequest(request);
+			reply = client.getReply();
+
+			client.close();
+			server.close();
+
+		} catch (IOException e) {
+			fail();
+		}
+		String nullString = "null";
+		assertEquals(reply, nullString);
+		assertEquals(outContent.toString(), "client connected\r\n"
+				+ "request: ADDREVIEW {\"type\": \"review\", \"business_id\": \"1CBs84C-a-cuA3vncXVSAw\", \"votes\": {\"cool\": 0, \"useful\": 0, \"funny\": 0}, \"review_id\": \"0f8QNSVSocn40zr1tSSGRw\", \"text\": \"Food here is very consistent and good for a quick lunch or dinner. I usually order the garlic bread and salad; however, their pizza, especially the bbq chicken, is pretty good too.\", \"stars\": 4, \"user_id\": \"7\", \"date\": \"2012-01-28\"}\r\n"
+				+ "ERR: NO_SUCH_USER\r\n");
+	}
+
+	@Test
+	public void serverTest10() throws IOException, InterruptedException {
+
+		String request = "ADDREVIEW {\"type\": \"review\", \"business_id\": \"2\", \"votes\": {\"cool\": 0, \"useful\": 0, \"funny\": 0}, \"review_id\": \"0f8QNSVSocn40zr1tSSGRw\", \"text\": \"Food here is very consistent and good for a quick lunch or dinner. I usually order the garlic bread and salad; however, their pizza, especially the bbq chicken, is pretty good too.\", \"stars\": 4, \"user_id\": \"wr3JF-LruJ9LBwQTuw7aUg\", \"date\": \"2012-01-28\"}";
+
+		ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+		System.setErr(new PrintStream(outContent));
+
+		String reply = null;
+		try {
+			final YelpDBServer server = new YelpDBServer(4949);
+			Thread serverThread = new Thread(() -> {
+				try {
+					server.serve();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			});
+			serverThread.start();
+
+			Client client = new Client("localhost", 4949);
+			client.sendRequest(request);
+			reply = client.getReply();
+
+			client.close();
+			server.close();
+
+		} catch (IOException e) {
+			fail();
+		}
+		String nullString = "null";
+		assertEquals(reply, nullString);
+		assertEquals(outContent.toString(), "client connected\r\n"
+				+ "request: ADDREVIEW {\"type\": \"review\", \"business_id\": \"2\", \"votes\": {\"cool\": 0, \"useful\": 0, \"funny\": 0}, \"review_id\": \"0f8QNSVSocn40zr1tSSGRw\", \"text\": \"Food here is very consistent and good for a quick lunch or dinner. I usually order the garlic bread and salad; however, their pizza, especially the bbq chicken, is pretty good too.\", \"stars\": 4, \"user_id\": \"wr3JF-LruJ9LBwQTuw7aUg\", \"date\": \"2012-01-28\"}\r\n"
+				+ "ERR: NO_SUCH_RESTAURANT\r\n");
+	}
+	
+	@Test
+	public void serverTest11() throws IOException, InterruptedException {
+
+		String request = "Git gud nerd";
+
+		ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+		System.setErr(new PrintStream(outContent));
+
+		String reply = null;
+		try {
+			final YelpDBServer server = new YelpDBServer(4949);
+			Thread serverThread = new Thread(() -> {
+				try {
+					server.serve();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			});
+			serverThread.start();
+
+			Client client = new Client("localhost", 4949);
+			client.sendRequest(request);
+			reply = client.getReply();
+
+			client.close();
+			server.close();
+
+		} catch (IOException e) {
+			fail();
+		}
+		String nullString = "";
+		assertEquals(reply, nullString);
+		assertEquals(outContent.toString(), "client connected\r\n"
+				+ "request: Git gud nerd\r\n"
+				+ "ERR: ILLEGAL_REQUEST\r\n");
 	}
 }
