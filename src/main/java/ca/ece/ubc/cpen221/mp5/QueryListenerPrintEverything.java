9c4f8ea409a8340ed83fb03f29ccd72487319349
Kota Chang <i@dongju.org>
2017-12-20 20:26:07 -0800

    treeCreater class edited

--- src/main/java/ca/ece/ubc/cpen221/mp5/QueryListenerPrintEverything.java
+++ src/main/java/ca/ece/ubc/cpen221/mp5/QueryListenerPrintEverything.java
@@ -1,28 +1,41 @@
 package ca.ece.ubc.cpen221.mp5;
 
-public class QueryListenerPrintEverything extends QueryBaseListener{
-	
-		public void andExprRoot (QueryParser.AndExprContext ctx) {
+public class QueryListenerPrintEverything extends QueryBaseListener {
+
+	public void andExprRoot(QueryParser.AndExprContext ctx) {
 		System.err.println("and expression");
-		}
-		public void exitRoot(QueryParser.InContext ctx) {
+	}
+
+	public void inRoot(QueryParser.InContext ctx) {
 		System.err.println("in");
-		}
-		public void enterNormal(QueryParser.IneqContext ctx) {
+	}
+
+	public void ineqRoot(QueryParser.IneqContext ctx) {
 		System.err.println("inequality");
-		}
-		public void exitNormal(QueryParser.NameContext ctx) {
+	}
+
+	public void nameRoot(QueryParser.NameContext ctx) {
 		System.err.println("name");
-		}
-		public void enterHtml(QueryParser.PriceContext ctx) {
+	}
+
+	public void priceRoot(QueryParser.PriceContext ctx) {
 		System.err.println("price");
-		}
-		public void exitHtml(QueryParser.OrExprContext ctx) {
+	}
+
+	public void orExprRoot(QueryParser.OrExprContext ctx) {
 		System.err.println("or expression");
-		}
-		public void enterItalic(QueryParser.AtomContext ctx) {
+	}
+
+	public void atomRoot(QueryParser.AtomContext ctx) {
 		System.err.println("atom");
-		}
-		
+	}
+
+	public void ratingRoot(QueryParser.RatingContext ctx) {
+		System.err.println("rating");
+	}
+	
+	public void categoryRoot(QueryParser.CategoryContext ctx) {
+		System.err.println("category");
+	}
 
 }
--- src/main/java/ca/ece/ubc/cpen221/mp5/QueryListenerTreeCreater.java
+++ src/main/java/ca/ece/ubc/cpen221/mp5/QueryListenerTreeCreater.java
@@ -9,15 +9,24 @@ import org.antlr.v4.runtime.tree.TerminalNode;
 
 import ca.ece.ubc.cpen221.mp5.QueryParser.AndExprContext;
 import ca.ece.ubc.cpen221.mp5.QueryParser.AtomContext;
+import ca.ece.ubc.cpen221.mp5.QueryParser.CategoryContext;
 import ca.ece.ubc.cpen221.mp5.QueryParser.InContext;
 import ca.ece.ubc.cpen221.mp5.QueryParser.IneqContext;
 import ca.ece.ubc.cpen221.mp5.QueryParser.NameContext;
 import ca.ece.ubc.cpen221.mp5.QueryParser.OrExprContext;
 import ca.ece.ubc.cpen221.mp5.QueryParser.PriceContext;
+import ca.ece.ubc.cpen221.mp5.QueryParser.RatingContext;
 
+/**
+ * Methods for the tree representation of the parsing
+ *
+ */
 public class QueryListenerTreeCreater extends QueryBaseListener {
-	//private Stack<Business> stack = new Stack<Business>();
+	private Stack<Business> stack = new Stack<Business>();
 	List<Business> businesses;
+	List<CategoryContext> categories;
+	List<RatingContext> ratings;
+	List<Review> reviews;
 
 	public QueryListenerTreeCreater() throws IOException {
 		String rest = "data/restaurants.json";
@@ -26,18 +35,24 @@ public class QueryListenerTreeCreater extends QueryBaseListener {
 		YelpDB db = new YelpDB(rest, user, review);
 		this.businesses = new ArrayList<Business>();
 		this.businesses.addAll(db.businesses);
+		categories = new ArrayList<CategoryContext>();
+		ratings = new ArrayList<RatingContext>();
+		reviews = new ArrayList<Review>();
 	}
 
 	public void exitAndExpr(AndExprContext ctx) {
-		
+		TerminalNode token = ctx.AND(0);
+		String text = token.getText();
 	}
 
 	public void exitOrExpr(OrExprContext ctx) {
-		
+		TerminalNode token = ctx.OR(0);
+		String text = token.getText();
 	}
 
 	public void exitIn(InContext ctx) {
-		
+		TerminalNode token = ctx.STRING();
+		String text = token.getText();
 	}
 
 	public void exitName(NameContext ctx) {
@@ -45,7 +60,7 @@ public class QueryListenerTreeCreater extends QueryBaseListener {
 		String text = token.getText();
 		for (int i=0; i< businesses.size(); i++) {
 			if (businesses.get(i).name().equals(text)) {
-				//stack.push(businesses.get(i));
+				
 			}
 		}
 	}
@@ -55,13 +70,23 @@ public class QueryListenerTreeCreater extends QueryBaseListener {
 		String text = token.getText();
 
 	}
+	
+	public void exitCategory(CategoryContext ctx) {
+		TerminalNode token = ctx.STRING();
+		String text = token.getText();
+	}
+	
+	public void extiRating(RatingContext ctx) {
+		TerminalNode token = ctx.NUM();
+		String text = token.getText();
+	}
 
 	public void exitAtom(AtomContext ctx) {
-
+		//TerminalNode token = ctx.addChild(matchedToken);
 	}
 
 	public void exitIneq(IneqContext ctx) {
-
+		TerminalNode token = ctx.EQ();
 	}
 
 	public void getQuery() {
